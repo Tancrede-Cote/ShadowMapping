6bdb27b9ed20ae6ec64728be5c7f180c4bb971df
Tancrede <tancrede.cote@telecom-paris.fr>
2026-02-19 14:12:56 +0100

    Revert "functional v1"
    This reverts commit c87cfaf894c60824d5c4792f9d796a6d4a0072fa.

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -91,7 +91,6 @@ void Mesh::addPlan(float square_half_side)
     glm::uvec3(_vertexPositions.size()-4, _vertexPositions.size()-2, _vertexPositions.size()-1));
 }
 
-#ifdef SUPPORT_OPENGL_45
 void Mesh::init()
 {
   glCreateBuffers(1, &_posVbo); // Generate a GPU buffer to store the positions of the vertices
@@ -127,8 +126,8 @@ void Mesh::init()
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ibo);
   glBindVertexArray(0); // Desactive the VAO just created. Will be activated at rendering time.
 }
-#else
-void Mesh::init()
+
+void Mesh::initOldGL()
 {
   // Generate a GPU buffer to store the positions of the vertices
   size_t vertexBufferSize = sizeof(glm::vec3)*_vertexPositions.size();
@@ -173,7 +172,6 @@ void Mesh::init()
 
   glBindVertexArray(0); // Desactive the VAO just created. Will be activated at rendering time.
 }
-#endif
 
 void Mesh::render()
 {
@@ -215,7 +213,7 @@ void loadOFF(const std::string &filename, std::shared_ptr<Mesh> meshPtr)
 {
   std::cout << " > Start loading mesh <" << filename << ">" << std::endl;
   meshPtr->clear();
-  std::ifstream in(filename.c_str());
+  std::ifstream in((filename).c_str());
   if(!in)
     throw std::ios_base::failure("[Mesh Loader][loadOFF] Cannot open " + filename);
   std::string offString;
